bc9dee9ca23bbffca73e932372f5480c38ac3e68
HashimHadizah <53875760+HashimHadizah@users.noreply.github.com>
2022-06-24 16:32:45 -0400

    my second commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//take the first operand input
+const number1 =parseInt(prompt('Enter first number: '));
+
+//take the operator input
+const operator = prompt('Enter operator(either +,-,* or /):');
+
+//take the second operand input
+const number2= parseInt(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if...else
+if (operator == '+') {
+    result = number1 + number2;
+
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*'){
+    result = number1 * number2;
+}
+
+else{
+    result = number1 / number2;
+}
+    
+//display the resultt
+alert(`${number1} $ {operator} $ {number2} = ${result}`)
+
